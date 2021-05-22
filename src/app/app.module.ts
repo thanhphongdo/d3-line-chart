7c92ba2f6635ca0614fd0cdc9bb1ffa617490ae9
Phong Do <phongdo@innoria.com>
2021-05-22 14:30:56 +0700

    guage chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { LineChartComponent } from './line-chart/line-chart.component';
 import { GuageChartComponent } from './guage-chart/guage-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LineChartComponent,
     GuageChartComponent
   ],
   imports: [
